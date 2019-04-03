89c5667465359396b0d33937571492d4f2652192
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-03 12:17:19 +0000

    Deploy techhahn/Travis-Integration to github.com/techhahn/Travis-Integration.git:gh-pages

---
+++ precache-manifest.9d05c2c878d03fa136361003ef166a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cd16d249172ba249ad6",
+    "url": "/Travis-Integration/static/css/main.4f057afc.chunk.css"
+  },
+  {
+    "revision": "3cd16d249172ba249ad6",
+    "url": "/Travis-Integration/static/js/main.dc202690.chunk.js"
+  },
+  {
+    "revision": "73d12f559056a74af8cf",
+    "url": "/Travis-Integration/static/js/runtime~main.b53e7b91.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Travis-Integration/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Travis-Integration/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a46ce0cd21a93eac93c208289dbf5560",
+    "url": "/Travis-Integration/index.html"
+  }
+];
